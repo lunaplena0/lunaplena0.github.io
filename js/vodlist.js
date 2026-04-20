e980e394f115a614066cb77b6f0ace28a0de071d
lunaplena0 <dalkkumli054@gmail.com>
2026-04-20 10:58:13 +0900

    Update vodlist.js

--- js/vodlist.js
+++ js/vodlist.js
@@ -807,9 +807,10 @@ function switchTab(id) {
     document.querySelectorAll('.tab-content').forEach(t => t.classList.remove('active'));
     document.querySelectorAll('.tab-btn').forEach(b => b.classList.remove('active'));
     document.getElementById(id).classList.add('active');
-    // event 객체가 있을 때만 실행
+    
     if (event && event.currentTarget) {
         event.currentTarget.classList.add('active');
+    }
 }
 
 window.onclick = function(event) {
