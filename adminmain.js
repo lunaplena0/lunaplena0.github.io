7c10918a5e2f7711cfcd149356f9fd89d45e64b1
lunaplena0 <dalkkumli054@gmail.com>
2026-08-07 16:14:46 +0900

    Update adminmain.js

--- adminmain.js
+++ adminmain.js
@@ -535,4 +535,3 @@ async function saveDataToWorker(fileType, contentObj, statusElementId) {
 function escapeHtml(str) {
     return (str || "").replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
 }
-```[cite: 1, 2]
